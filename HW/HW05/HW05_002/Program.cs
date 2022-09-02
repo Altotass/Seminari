cbf6dc17d3e85d7886b3334606cd31b80781cf26
altotass <altotass@gmail.com>
2022-09-02 17:23:25 +0300

    HW05_002

--- HW/HW05/HW05_002/Program.cs
+++ HW/HW05/HW05_002/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int x = Convert.ToInt32(Console.ReadLine());
+int [] massiv = new int[x];
+int sum = 0;
+QQQ (massiv);
+    for(int j = 0; j < x; j = j+2)
+    {
+    sum = sum + massiv[j];
+    }
+    Console.WriteLine();
+    Console.WriteLine("Сумма цифр = " + sum);
+void QQQ (int[] massiv)   // функция заполнения массива
+{ 
+    Console.Write($"Полученный массив ");
+    for(int i = 0; i < x; i++)
+    {
+    massiv[i] = new Random().Next(-10,10);
+    Console.Write($"{massiv[i]},");
+    }
+}
+
