f40ceedc082afb6c320c1477a092c23c56539f5a
altotass <altotass@gmail.com>
2022-09-02 11:25:28 +0300

    HW05_001

--- HW/HW05/HW05_001/Program.cs
+++ HW/HW05/HW05_001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива ");
+int x = Convert.ToInt32(Console.ReadLine());
+int [] mass = new int[x];
+Random r = new Random();
+int count = 0;
+QQQ(mass);
+        Console.WriteLine();
+        Console.WriteLine("=======================================");
+            for (int i = 0; i < mass.Length; i++)
+                if (mass[i] %2 == 0)
+                count++;
+                    Console.WriteLine($"В массиве {x} чисел, из них чётных {count}");
+                    Console.WriteLine("=======================================");
+
+void QQQ (int[] mass)
+{
+        for(int i = 0; i < x; i++)
+        {
+        mass[i] = r.Next(100,1000);
+        Console.Write($"{mass[i]},");
+        }
+}
