7ec96f878f12953426fc5f11372cc686cd80156e
altotass <altotass@gmail.com>
2022-09-03 07:45:41 +0300

    HW05_002

--- HW/HW05/HW05_002/Program.cs
+++ HW/HW05/HW05_002/Program.cs
@@ -3,24 +3,25 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+void TTT()
+{
 Console.WriteLine("Введите размер массива");
 int x = Convert.ToInt32(Console.ReadLine());
 int [] massiv = new int[x];
 int sum = 0;
 QQQ (massiv);
     for(int j = 0; j < x; j = j+2)
-    {
-    sum = sum + massiv[j];
+    {     sum = sum + massiv[j];
     }
     Console.WriteLine();
-    Console.WriteLine("Сумма цифр = " + sum);
+    Console.WriteLine("Сумма цифр на нечетных позициях = " + sum);
+    return;
 void QQQ (int[] massiv)   // функция заполнения массива
-{ 
-    Console.Write($"Полученный массив ");
+{     Console.Write($"Полученный массив ");
     for(int i = 0; i < x; i++)
-    {
-    massiv[i] = new Random().Next(-10,10);
+    {     massiv[i] = new Random().Next(-10,10);
     Console.Write($"{massiv[i]},");
     }
 }
-
+}
+TTT ();
