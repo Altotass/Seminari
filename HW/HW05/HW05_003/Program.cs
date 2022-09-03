2b4142e370650e5cd883be13d228d35a6f5e7144
altotass <altotass@gmail.com>
2022-09-03 07:39:09 +0300

    HW05_003

--- HW/HW05/HW05_003/Program.cs
+++ HW/HW05/HW05_003/Program.cs
@@ -2,27 +2,28 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Введите размер массива ");
+void EEE()
+{ Console.WriteLine("Введите размер массива ");
 int x = Convert.ToInt32(Console.ReadLine());
 double [] massiv = new double[x];
 double NumMin = Int32.MaxValue;  // ????? Вопрос преподавателю!!! если ставлю massiv[0] , то получаю NumMin = 0
 double NumMax = massiv[0];
 QQQ (massiv);
     for(int i = 0; i < x; i++)
-        {
-        if (NumMin > massiv[i]) NumMin = massiv[i];
+        {         if (NumMin > massiv[i]) NumMin = massiv[i];
         if (NumMax < massiv[i]) NumMax = massiv[i];
         }
         Console.WriteLine($" Минимальное число = {NumMin}");
         Console.WriteLine($" Максимальное число = {NumMax}");
         Console.WriteLine($" Разница между ними = " + (NumMax - NumMin));
+        return;
 void QQQ (double[] massiv)   // функция заполнения массива
-{ 
-    Console.Write($"Полученный массив  ");
+    {    Console.Write($"Полученный массив  ");
     for(int i = 0; i < x; i++)
-    {
-    massiv[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;  // Превратили в вещественный вид
+    {    massiv[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;  // Превратили в вещественный вид
     Console.Write($"{massiv[i]}; ");
     }
     Console.WriteLine();
+    }
 }
+EEE();
